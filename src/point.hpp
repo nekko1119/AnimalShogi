123692e1b3a12394576d6db1f4fd24e6c6fd8377
nekko1119 <taku50501119@gmail.com>
2013-11-01 17:22:05 +0900

    座標を表すpointクラスの作成

--- src/point.hpp
+++ src/point.hpp
@@ -1,11 +1,92 @@
 ﻿#ifndef ANIMAL_SHOGI_POINT_HPP
 #define NAIMAL_SHOGI_POINT_HPP
 
-#include <boost/graph/simple_point.hpp>
+#include <type_traits>
 
 namespace animal_shogi
 {
-    using point = boost::simple_point<int>;
+    template <typename T>
+    struct basic_point
+    {
+        static_assert(std::is_arithmetic<T>::value, "basic_point<T> : T must be arithmetic type");
+    public:
+        //explicitにしない
+        basic_point(T val)
+            : basic_point(val, val)
+        {
+        }
+
+        basic_point(T x, T y)
+            : x_(x), y_(y)
+        {
+        }
+
+        T x() const
+        {
+            return x_;
+        }
+
+        void x(T val)
+        {
+            x_ = val;
+        }
+
+        T y() const
+        {
+            return y_;
+        }
+
+        void y(T val)
+        {
+            y_ = val;
+        }
+
+        basic_point& operator+=(basic_point const& rhs)
+        {
+            x_ += rhs.x_;
+            y_ += rhs.y_;
+            return *this;
+        }
+
+        basic_point& operator-=(basic_point const& rhs)
+        {
+            x_ -= rhs.x_;
+            y_ -= rhs.y_;
+            return *this;
+        }
+
+    private:
+        T x_;
+        T y_;
+    };
+
+    template <typename T>
+    bool operator==(basic_point<T> const& lhs, basic_point<T> const& rhs)
+    {
+        return lhs.x() == rhs.x() && lhs.y() == rhs.y();
+    }
+
+    template <typename T>
+    bool operator!=(basic_point<T> const& lhs, basic_point<T> const& rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    template <typename T>
+    basic_point<T> operator+(basic_point<T> lhs, basic_point<T> const& rhs)
+    {
+        lhs += rhs;
+        return lhs;
+    }
+
+    template <typename T>
+    basic_point<T> operator-(basic_point<T> lhs, basic_point<T> const& rhs)
+    {
+        lhs -= rhs;
+        return lhs;
+    }
+
+    using point = basic_point<int>;
 }
 
 #endif
---
+++ src/test/point_test.cpp
@@ -0,0 +1,76 @@
+﻿#include "point.hpp"
+#include <gtest/gtest.h>
+using namespace animal_shogi;
+
+TEST(point_test, exapmle)
+{
+    point p{1, 2};
+    EXPECT_EQ(1, p.x());
+    p.y(3);
+    p += 2;
+    EXPECT_TRUE(p == point(3, 5));
+}
+
+TEST(point_test, constuct)
+{
+    point p{1, 2};
+    EXPECT_EQ(1, p.x());
+    EXPECT_EQ(2, p.y());
+}
+
+TEST(point_test, copy_ctor)
+{
+    point p1{1, 2};
+    point p2{p1};
+    EXPECT_EQ(1, p2.x());
+    EXPECT_EQ(2, p2.y());
+}
+
+TEST(point_test, assignment)
+{
+    point p1{1, 2};
+    point p2{3, 4};
+    p2 = p1;
+    EXPECT_EQ(1, p2.x());
+    EXPECT_EQ(2, p2.y());
+}
+
+TEST(point_test, accessor)
+{
+    point p{1, 2};
+    EXPECT_EQ(1, p.x());
+    EXPECT_EQ(2, p.y());
+    p.x(3);
+    EXPECT_EQ(3, p.x());
+    p.y(4);
+    EXPECT_EQ(4, p.y());
+}
+
+TEST(point_test, compare)
+{
+    point p1{1, 2};
+    point p2{3, 4};
+    EXPECT_FALSE(p1 == p2);
+    EXPECT_TRUE(p1 != p2);
+    p2 = p1;
+    EXPECT_TRUE(p1 == p2);
+    EXPECT_FALSE(p1 != p2);
+}
+
+TEST(point_test, add)
+{
+    point p1{1, 2};
+    point p2{3, 4};
+    p1 += p2;
+    EXPECT_EQ(p1, point(4, 6));
+    EXPECT_EQ(p1 + p2, point(7, 10));
+}
+
+TEST(point_test, sub)
+{
+    point p1{1, 2};
+    point p2{3, 4};
+    p1 -= p2;
+    EXPECT_EQ(p1, point(-2, -2));
+    EXPECT_EQ(p1 - p2, point(-5, -6));
+}
