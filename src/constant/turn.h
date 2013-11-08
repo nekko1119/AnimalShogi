e92afadd2972fbe1f135000145724478120bf96b
nekko1119 <taku50501119@gmail.com>
2013-11-08 15:50:43 +0900

    列挙名をplayerからturnに変更

---
+++ src/constant/turn.h
@@ -0,0 +1,13 @@
+﻿#ifndef ANIMAL_SHOGI_CONSTANT_TURN_H
+#define ANIMAL_SHOGI_CONSTANT_TURN_H
+
+namespace animal_shogi
+{
+    enum class turn
+    {
+        BLACK,  // 先手
+        WHITE   // 後手
+    };
+}
+
+#endif
