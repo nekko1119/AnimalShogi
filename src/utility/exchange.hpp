c873874a8d0076df5430865bb3ecf0696f43479f
nekko1119 <taku50501119@gmail.com>
2013-11-16 19:53:29 +0900

    utilityにexchange関数を追加

---
+++ src/utility/exchange.hpp
@@ -0,0 +1,20 @@
+﻿// C++14で入るstd::exchange関数
+#ifndef ANIMAL_SHOGI_UTILITY_EXCHANGE_HPP
+#define ANIMAL_SHOGI_UTILITY_EXCHANGE_HPP
+
+#include <type_traits>
+#include <utility>
+
+namespace animal_shogi
+{
+    template <typename T, typename U = T>
+    std::enable_if_t<std::is_move_assignable<T>::value, T>
+        exchange(std::remove_reference_t<T>& obj, std::remove_reference_t<U>&& new_value)
+    {
+        T old_value = std::move(obj);
+        obj = std::forward<U>(new_value);
+        return old_value;
+    }
+}
+
+#endif
