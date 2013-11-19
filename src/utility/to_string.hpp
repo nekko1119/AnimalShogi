9a5b0b29717718484cf262c86edc1ff45362db1e
nekko1119 <taku50501119@gmail.com>
2013-11-20 07:39:09 +0900

    to_string追加
    ユーザー定義クラスを文字列に変換する際は、この関数テンプレートの特殊化を書く

---
+++ src/utility/to_string.hpp
@@ -0,0 +1,14 @@
+﻿#ifndef ANIMAL_SHOGI_UTILITY_TO_STRING_HPP
+#define ANIMAL_SHOGI_UTILITY_TO_STRING_HPP
+
+#include <string>
+#include <type_traits>
+#include <utility>
+
+namespace animal_shogi
+{
+    template <typename T>
+    inline std::string to_string(std::remove_reference_t<T>&& value) = delete;
+}
+
+#endif
