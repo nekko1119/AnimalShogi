8642a338e09d67c11afe3cd0673bcdb902f21065
nekko1119 <taku50501119@gmail.com>
2013-11-25 19:33:23 +0900

    指し手を表すmovement構造体と、棋譜を表すrecordクラスの実装

---
+++ src/record.h
@@ -0,0 +1,65 @@
+﻿#ifndef ANIMAL_SHOGI_RECORD_H
+#define ANIMAL_SHOGI_RECORD_H
+
+#include "config.hpp"
+#include "piece.h"
+#include "utility/point.hpp"
+#include <boost/optional.hpp>
+#include <string>
+#include <vector>
+#include <utility>
+
+namespace animal_shogi
+{
+    // 指し手を表す構造体
+    struct movement
+    {
+        movement(boost::optional<animal_shogi::point> from, animal_shogi::point to, piece pc) BOOST_NOEXCEPT;
+        movement(movement const&) BOOST_NOEXCEPT = default;
+        movement(movement&& rhs) BOOST_NOEXCEPT;
+        movement& operator=(movement rhs) BOOST_NOEXCEPT;
+        void swap(movement& rhs) BOOST_NOEXCEPT;
+        std::string str() const;
+
+        // 元の座標
+        // 持ち駒打ちの時、noneとなる
+        boost::optional<animal_shogi::point> from;
+        // 向かう場所
+        animal_shogi::point to;
+        // 駒の種類
+        animal_shogi::piece pc;
+    };
+
+    void swap(movement& lhs, movement& rhs) BOOST_NOEXCEPT;
+
+    // 棋譜を表すクラス
+    // // recordクラスで棋譜自体の整合性のチェックは行わない
+    class record
+    {
+    public:
+        using container_type = std::vector<movement const>;
+        using value_type = container_type::value_type;
+        using reference = container_type::reference;
+        using const_reference = container_type::const_reference;
+        using size_type = container_type::size_type;
+        using iterator = container_type::iterator;
+        using const_iterator = container_type::const_iterator;
+
+        void push(boost::optional<point> from, point to, piece pc);
+        void pop();
+        size_type size() const;
+        reference top();
+        const_reference top() const;
+        iterator begin();
+        const_iterator begin() const;
+        iterator end();
+        const_iterator end() const;
+        reference operator[](size_type index);
+        const_reference operator[](size_type index) const;
+
+    private:
+        container_type moves_;
+    };
+}
+
+#endif
---
+++ src/test/record_test.cpp
@@ -0,0 +1,149 @@
+﻿#include <record.h>
+#include <gtest/gtest.h>
+#include <algorithm>
+using namespace animal_shogi;
+
+namespace
+{
+    struct compare_t
+    {
+        template <typename T>
+        bool operator()(T const& lhs, T const& rhs) const
+        {
+            return lhs.from == rhs.from && lhs.to == rhs.to && lhs.pc == rhs.pc;
+        };
+    } compare;
+}
+
+TEST(record_test, example)
+{
+    record r;
+    r.push(point{2, 3}, {2, 2}, {turn::BLACK, ptype::CHICK});
+    r.push(point{1, 1}, {2, 2}, {turn::WHITE, ptype::ELEPHANT});
+    r.push(boost::none, {2, 3}, {turn::BLACK, ptype::CHICK});
+    std::cout << r[0].str() << std::endl;
+    std::cout << r[1].str() << std::endl;
+    std::cout << r[2].str() << std::endl;
+    for (auto const& it : r)
+    {
+        std::cout << it.str() << std::endl;
+    }
+    r.pop();
+    auto const& m = r.top();
+    EXPECT_TRUE(*m.from == point(1, 1));
+}
+
+TEST(record_test, construct)
+{
+    record r;
+    r.pop();
+    //r[0].str(); // assert failure.
+    r.begin();
+    //r.end(); // assert failure
+    //auto const& m = r.top(); // assert failure
+    EXPECT_EQ(0, r.size());
+}
+
+TEST(record_test, push)
+{
+    record r;
+    // recordクラスで棋譜自体の整合性のチェックは行わない
+    r.push(point{2, 3}, {2, 2}, {turn::BLACK, ptype::CHICK});
+    r.push(point{1, 1}, {2, 2}, {turn::WHITE, ptype::ELEPHANT});
+    r.push(boost::none, {2, 3}, piece{turn::BLACK, ptype::CHICK});
+    std::vector<movement> expected =
+    {
+        {point{2, 3}, {2, 2}, {turn::BLACK, ptype::CHICK}},
+        {point{1, 1}, {2, 2}, {turn::WHITE, ptype::ELEPHANT}},
+        {boost::none, {2, 3}, {turn::BLACK, ptype::CHICK}}
+    };
+    EXPECT_EQ(3, r.size());
+    bool cond = std::equal(std::begin(r), std::end(r), std::begin(expected), compare);
+    EXPECT_TRUE(cond);
+}
+
+TEST(record_test, pop)
+{
+    record r;
+    r.push(point{2, 3}, {2, 2}, {turn::BLACK, ptype::CHICK});
+    r.push(point{1, 1}, {2, 2}, {turn::WHITE, ptype::ELEPHANT});
+    r.push(boost::none, {2, 3}, {turn::BLACK, ptype::CHICK});
+    std::vector<movement> expected =
+    {
+        {point{2, 3}, {2, 2}, {turn::BLACK, ptype::CHICK}},
+        {point{1, 1}, {2, 2}, {turn::WHITE, ptype::ELEPHANT}},
+        {boost::none, {2, 3}, {turn::BLACK, ptype::CHICK}}
+    };
+    EXPECT_EQ(3, r.size());
+
+    r.pop();
+    EXPECT_EQ(2, r.size());
+    expected.pop_back();
+    bool cond = std::equal(std::begin(r), std::end(r), std::begin(expected), compare);
+    EXPECT_TRUE(cond);
+
+    r.push(point{3, 1}, {3, 2}, {turn::WHITE, ptype::GIRAFFE});
+    EXPECT_EQ(3, r.size());
+    expected.emplace_back(point{3, 1}, point{3, 2}, piece{turn::WHITE, ptype::GIRAFFE});
+    cond = std::equal(std::begin(r), std::end(r), std::begin(expected), compare);
+    EXPECT_TRUE(cond);
+}
+
+TEST(record_test, size)
+{
+    record r;
+    EXPECT_EQ(0, r.size());
+    r.push(point{0, 0}, {0, 0}, {turn::BLACK, ptype::CHICK});
+    EXPECT_EQ(1, r.size());
+    r.pop();
+    EXPECT_EQ(0, r.size());
+}
+
+TEST(record_test, top)
+{
+    record r;
+    //auto const& m = r.top(); // assert failure
+    movement m{point{0, 0}, {0, 0}, {turn::WHITE, ptype::LION}};
+    movement m2{point{3, 3}, {3, 4}, {turn::BLACK, ptype::ELEPHANT}};
+    r.push(m.from, m.to, m.pc);
+    EXPECT_TRUE(compare(r.top(), m));
+    r.push(m2.from, m2.to, m2.pc);
+    EXPECT_TRUE(compare(r.top(), m2));
+}
+
+TEST(record_test, iterator)
+{
+    record r;
+    movement m[] =
+    {
+        {point{2, 3}, {2, 2}, {turn::BLACK, ptype::CHICK}},
+        {point{1, 1}, {2, 2}, {turn::WHITE, ptype::ELEPHANT}},
+        {boost::none, {2, 3}, {turn::BLACK, ptype::CHICK}}
+    };
+    r.push(point{2, 3}, {2, 2}, {turn::BLACK, ptype::CHICK});
+    r.push(point{1, 1}, {2, 2}, {turn::WHITE, ptype::ELEPHANT});
+    r.push(boost::none, {2, 3}, {turn::BLACK, ptype::CHICK});
+    int i = 0;
+    for (auto it = r.begin(), last = r.end(); it != last; ++it, ++i)
+    {
+        EXPECT_TRUE(compare(m[i], *it));
+    }
+}
+
+TEST(record_test, ramdom_access)
+{
+    record r;
+    movement m[] =
+    {
+        {point{2, 3}, {2, 2}, {turn::BLACK, ptype::CHICK}},
+        {point{1, 1}, {2, 2}, {turn::WHITE, ptype::ELEPHANT}},
+        {boost::none, {2, 3}, {turn::BLACK, ptype::CHICK}}
+    };
+    r.push(point{2, 3}, {2, 2}, {turn::BLACK, ptype::CHICK});
+    r.push(point{1, 1}, {2, 2}, {turn::WHITE, ptype::ELEPHANT});
+    r.push(boost::none, {2, 3}, {turn::BLACK, ptype::CHICK});
+    for (record::size_type i = 0; i < r.size(); ++i)
+    {
+        EXPECT_TRUE(compare(m[i], r[i]));
+    }
+}
