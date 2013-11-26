20611eeec67d763abf49506cf2766980f2130c7b
nekko1119 <taku50501119@gmail.com>
2013-11-26 18:42:03 +0900

    持ち駒のライオンを表示しないように変更

--- src/captured_piece.cpp
+++ src/captured_piece.cpp
@@ -1,7 +1,7 @@
-﻿#include "captured_piece.h"
+﻿#include "config.hpp"
+#include "captured_piece.h"
 #include <type_traits>
 #include <boost/log/trivial.hpp>
-#include "config.hpp"
 using namespace animal_shogi;
 
 void captured_piece::add(ptype p)
@@ -51,7 +51,7 @@ int captured_piece::get(ptype p) const
 std::string captured_piece::str() const
 {
     std::string str;
-    auto const last = pieces_.size();
+    auto const last = pieces_.size() - 1;
     for (ASHOGI_DECLTYPE_N3276(pieces_)::size_type i = 0; i < last; ++i)
     {
         str += to_string(static_cast<ptype>(i)) + " " + std::to_string(pieces_[i]) + ", ";
