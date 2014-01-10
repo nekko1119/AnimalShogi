156e93268717ddd718ff15ab0573faa11e8c94f9
nekko1119 <taku50501119@gmail.com>
2014-01-10 17:08:34 +0900

    例外処理を追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
-﻿#include "game.hpp"
+﻿#include <iostream>
+#include "game.hpp"
 #include "player/human.h"
 #include "utility/logging.h"
 
 int main()
 {
-    auto log = animal_shogi::logging{};
-    animal_shogi::game<animal_shogi::human, animal_shogi::human>{}();
+    try
+    {
+        auto log = animal_shogi::logging{};
+        animal_shogi::game<animal_shogi::human, animal_shogi::human>{}();
+    }
+    catch (std::exception const& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "unknown exception" << std::endl;
+    }
 }
