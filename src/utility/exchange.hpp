6201420247c3ec03a3a3918d49fac97b534bf8e9
nekko1119 <taku50501119@gmail.com>
2014-01-15 12:41:04 +0900

    シンプルな実装に修正

--- src/utility/exchange.hpp
+++ src/utility/exchange.hpp
@@ -8,8 +8,7 @@
 namespace animal_shogi
 {
     template <typename T, typename U = T>
-    inline std::enable_if_t<std::is_move_assignable<T>::value, T>
-        exchange(std::remove_reference_t<T>& obj, std::remove_reference_t<U>&& new_value)
+    inline T exchange(T& obj, U&& new_value)
     {
         T old_value = std::move(obj);
         obj = std::forward<U>(new_value);
